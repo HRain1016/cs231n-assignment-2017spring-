6b25e13778da6d7b33a6ba4e42386f7f467a19bd
Aubrey <1436772622@qq.com>
2019-01-17 16:21:12 +0800

    Signed-off-by: Aubrey <1436772622@qq.com>

--- assignment1/cs231n/features.py
+++ assignment1/cs231n/features.py
@@ -21,29 +21,29 @@ def extract_features(imgs, feature_fns, verbose=False):
   - verbose: Boolean; if true, print progress.
 
   Returns:
-  An array of shape (N, F_1 + ... + F_k) where each column is the concatenation
+  An array of shape (N, F_1 + ... + F_k) where each row is the concatenation
   of all features for a single image.
   """
   num_images = imgs.shape[0]
   if num_images == 0:
     return np.array([])
 
-  # Use the first image to determine feature dimensions
+  # Use the first image to determine feature dimensions # 使用第一张图片决定维度
   feature_dims = []
   first_image_features = []
   for feature_fn in feature_fns:
-    feats = feature_fn(imgs[0].squeeze())
-    assert len(feats.shape) == 1, 'Feature functions must be one-dimensional'
-    feature_dims.append(feats.size)
-    first_image_features.append(feats)
+    feats = feature_fn(imgs[0].squeeze())  # 把shape中为1的维度去掉
+    assert len(feats.shape) == 1, 'Feature functions must be one-dimensional'# 每个方法得到的特征都是一个向量
+    feature_dims.append(feats.size)  # 特征向量长度列表
+    first_image_features.append(feats)  # 特征向量列表
 
   # Now that we know the dimensions of the features, we can allocate a single
   # big array to store all features as columns.
-  total_feature_dim = sum(feature_dims)
-  imgs_features = np.zeros((num_images, total_feature_dim))
-  imgs_features[0] = np.hstack(first_image_features).T
+  total_feature_dim = sum(feature_dims)  # 所有特征的长度加起来作为图像特征矩阵的列数
+  imgs_features = np.zeros((num_images, total_feature_dim))  # 图像特征矩阵的行数为图像数目
+  imgs_features[0] = np.hstack(first_image_features).T  # 图像矩阵的第一行已经求出来了，将所有特征向量列表按行组合在一起即可
 
-  # Extract features for the rest of the images.
+  # Extract features for the rest of the images.# 提取剩余图片的特征
   for i in xrange(1, num_images):
     idx = 0
     for feature_fn, feature_dim in zip(feature_fns, feature_dims):
@@ -60,13 +60,13 @@ def rgb2gray(rgb):
   """Convert RGB image to grayscale
 
     Parameters:
-      rgb : RGB image
+      rgb : RGB image (m x n x 3) m*n个RGB像素
 
     Returns:
-      gray : grayscale image
+      gray : grayscale image （m x n rgb变为灰度）
   
   """
-  return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
+  return np.dot(rgb[...,:3], [0.299, 0.587, 0.144]) # 会将最内层的数据截断成3并和第二个数组相乘，结果rgb三个值变为一个灰度值
 
 
 def hog_feature(im):
@@ -88,10 +88,10 @@ def hog_feature(im):
   """
   
   # convert rgb to grayscale if needed
-  if im.ndim == 3:
-    image = rgb2gray(im)
+  if im.ndim == 3:  # dim返回数组的维度，比如5x3x3 维度为3
+    image = rgb2gray(im)  # 执行完会降维，比如变为5x3
   else:
-    image = np.at_least_2d(im)
+    image = np.atleast_2d(im) # 将数组至少变成2维,高于2维保持原样
 
   sx, sy = image.shape # image size
   orientations = 9 # number of gradient bins
@@ -118,7 +118,7 @@ def hog_feature(im):
     # select magnitudes for those orientations
     cond2 = temp_ori > 0
     temp_mag = np.where(cond2, grad_mag, 0)
-    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx/2::cx, cy/2::cy].T
+    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx//2::cx, cy//2::cy].T
   
   return orientation_histogram.ravel()
 
